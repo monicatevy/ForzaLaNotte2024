5dcf8595bc6111e8fe704c182f668b19a54913aa
minhtripham <minhtripham9>
2024-12-05 23:57:26 +0100

    modify route

--- app.py
+++ app.py
@@ -5,10 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def maze_input():
-    return render_template('index.html')
-
-@app.route('/memory-game')
-def memory_game():
     return render_template('memory_game.html')
 
 
